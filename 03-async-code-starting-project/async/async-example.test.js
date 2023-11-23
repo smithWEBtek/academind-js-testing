8b193a7bca68002986ed76000b180d26299ea30f
brad@smithwebtek.com <brad@smithwebtek.com>
2023-11-23 10:13:21 -0500

    Add instructions to async example test

--- 03-async-code-starting-project/async/async-example.test.js
+++ 03-async-code-starting-project/async/async-example.test.js
@@ -45,3 +45,14 @@ it("should return an error if email is blank", (done) => {
     }
   });
 });
+
+// Even though the test worked as expected in the previous lecture, you should actually return the promise assertion in your tests:
+
+// it('should generate a token value', () => {
+//  const testUserEmail = 'test@test.com';
+ 
+//  return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+// });
+// This guarantees that Vitest / Jest wait for the promise to be resolved.
+
+// You don't need to return when using async / await (since a function annotated with async returns a promise implicitly).
