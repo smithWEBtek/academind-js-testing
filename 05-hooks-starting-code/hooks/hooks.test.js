b1f8c58df34529eafe9d038415d600aef0c9affd
brad@smithwebtek.com <brad@smithwebtek.com>
2023-11-23 11:28:39 -0500

    Complete hooks section

--- 05-hooks-starting-code/hooks/hooks.test.js
+++ 05-hooks-starting-code/hooks/hooks.test.js
@@ -1,4 +1,4 @@
-import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from "vitest";
+import { it, expect, beforeAll, beforeEach, afterEach, afterAll, describe } from "vitest";
 import { User } from "./hooks";
 
 const testEmail = "test@test.com";
@@ -22,27 +22,36 @@ afterAll(() => {
   console.log('afterAll');
 })
 
+describe('hooks', () => {
 
-it("should update the email", () => {
-  const newTestEmail = "test2@test.com";
-  user.updateEmail(newTestEmail);
-  expect(user.email).toBe(newTestEmail);
-});
+  it("should update the email", () => {
+    const newTestEmail = "test2@test.com";
+    user.updateEmail(newTestEmail);
+    expect(user.email).toBe(newTestEmail);
+  });
+  
+  it("should have an email property", () => {
+    expect(user).toHaveProperty("email");
+  });
+  
+  it("should store the provided email value", () => {
+    expect(user.email).toBe(testEmail);
+  });
+  
+  it("should clear the email", () => {
+    user.clearEmail();
+    expect(user.email).toBe("");
+  });
+  
+  it("should still have an email property after clearing the email", () => {
+    user.clearEmail();
+    expect(user).toHaveProperty("email");
+  });
+  
+})
 
-it("should have an email property", () => {
-  expect(user).toHaveProperty("email");
-});
+// Even when not adding the .concurrent property / annotation, tests that are stored in different files are executed concurrently (i.e., in parallel). This is done by both Vitest and Jest - ensuring that your tests run in a short amount of time.
 
-it("should store the provided email value", () => {
-  expect(user.email).toBe(testEmail);
-});
+// With .concurrent you can enforce this behavior also inside the individual files (i.e., tests that live in one and the same file are executed concurrently).
 
-it("should clear the email", () => {
-  user.clearEmail();
-  expect(user.email).toBe("");
-});
-
-it("should still have an email property after clearing the email", () => {
-  user.clearEmail();
-  expect(user).toHaveProperty("email");
-});
+// Concurrent execution can reduce the amount of time your tests need to execute. A downside of concurrent execution is, that tests that perform clashing (global) state manipulations may interfere with each other.
