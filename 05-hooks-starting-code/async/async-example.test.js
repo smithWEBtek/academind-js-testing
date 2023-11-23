ed87edece67de8164a89411f306fe255fc8e2690
brad@smithwebtek.com <brad@smithwebtek.com>
2023-11-23 10:46:26 -0500

    Restore original folder, update async folder

--- 05-hooks-starting-code/async/async-example.test.js
+++ 05-hooks-starting-code/async/async-example.test.js
@@ -1,16 +1,17 @@
-import { expect, it } from 'vitest';
+import { it, expect, doneFn } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
 
-import { generateToken, generateTokenPromise } from './async-example';
-
-it('should generate a token value', (done) => {
-  const testUserEmail = 'test@test.com';
-
-  generateToken(testUserEmail, (err, token) => {
+it("should generate a token value: callback version", (done) => {
+  const email = "brad@smithwebtek.com";
+  generateToken(email, (err, token) => {
+    // *** short version:
     // expect(token).toBeDefined();
+    // done();
 
+    // *** version w/ good fail, w/msg output
     try {
       expect(token).toBeDefined();
-      // expect(token).toBe(2);
+      //   expect(token).toBe(2)
       done();
     } catch (err) {
       done(err);
@@ -18,16 +19,40 @@ it('should generate a token value', (done) => {
   });
 });
 
-it('should generate a token value', () => {
-  const testUserEmail = 'test@test.com';
-
-  expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+it("should generate a token value: Promise version", () => {
+  const email = "brad@smithwebtek.com";
+  // wrap the function which returns a Promise, in expect
+  expect(generateTokenPromise(email)).resolves.toBeDefined();
+  //   expect(generateTokenPromise(email)).resolves.toBe(2); // fails with helpful msg
 });
 
-it('should generate a token value', async () => {
-  const testUserEmail = 'test@test.com';
-
-  const token = await generateTokenPromise(testUserEmail);
-
-  expect(token).toBeDefined();
+it("should generate a token value: async/await version", async () => {
+  // using async/await, "awaits" the function to run, then the expectation can run
+  const email = "brad@smithwebtek.com";  
+  const tokenPromise = await generateTokenPromise(email)
+  expect(tokenPromise).toBeDefined();
 });
+
+it("should return an error if email is blank", (done) => {
+  const email = "";
+  generateToken(email, (err, token) => {
+    try {
+      expect(err).toBeDefined();
+      expect(err).toBeNull();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+});
+
+// Even though the test worked as expected in the previous lecture, you should actually return the promise assertion in your tests:
+
+// it('should generate a token value', () => {
+//  const testUserEmail = 'test@test.com';
+ 
+//  return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+// });
+// This guarantees that Vitest / Jest wait for the promise to be resolved.
+
+// You don't need to return when using async / await (since a function annotated with async returns a promise implicitly).
